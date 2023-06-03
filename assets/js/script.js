3c27f8212d97ecc1523702e2920a966577bee169
pakuhn <pakuhn93@gmail.com>
2023-06-03 01:34:42 -0700

    completed localStorage functionality, moving onto more conditionals

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,25 +66,6 @@ var displayDate = setInterval(timer => {
 
 }, 1000);
 
-
-//create the element
-//set the attributes
-//set the text
-//put it on the page
-
-
-// function generateTimes(){
-//   var startTime = 9; // military time, must be in same day
-//   var endTime = 17;
-//   var timeRange = (endTime - startTime); // business hours in a day
-//   var timeArr = [];
-//   for (i = 0; i < timeRange; i++){
-    
-//   }
-// }
-
-// generateTimes();
-
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -92,28 +73,11 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  //stores user information onto local storage after clicking the save button
   saveBtn.click(function saveMySchedule() {
-    // var cardText = $(this).siblings('.description').val();
-    // var cardPast = $(this).parent().class('past');
-    // var cardPresent = $(this).parent().class('present');
-    // var cardFuture = $(this).parent().class('future');
-    // currentDateAndTime = dayjs().format(dateAndTimeFormat);
-    
-    // if(cardId == 'hour-9'){
-    //   myData.past = cardText;
-    // } else if (cardId == 'hour-10'){
-    //   myData.present = cardText;
-    // } else if (cardId == 'hour-11'){
-    //   myData.future = cardText;
-    // } else { console.log("ERROR???") }
-
-    // console.log(cardId);
-    var storeInfo = JSON.stringify(myData);
-
-    console.log(storeInfo);
-    
-
-    
+    var storageInfo = JSON.stringify(myData);
+    localStorage.setItem(myLocalStorage, storageInfo);
   })
   
   //
